93e568a207318daa699a68c87fd66b31383fb830
212703953 <vianney.mixtur@ge.com>
2019-09-01 22:00:43 +0200

    Added update factory

--- src/backend/factory/Factory.js
+++ src/backend/factory/Factory.js
@@ -2,15 +2,21 @@ var db = require("../db");
 
 var Factory = {
   getfactories: function(callback) {
-    return db.query("SELECT * from factory", callback);
+    return db.query("SELECT * from factory", callback)
   },
   createfactory: function(factory, callback) {
     return db.query(
       "Insert into factory(id, name, subbusiness, shift1, shift2, shift3) values(?, ?, ?, ?,?,?)",
-      [factory.id, factory.name, factory.subbusiness, factory.shift1, factory.shift2, factory.shift3],
+      [factory.id, factory.name, factory.business, factory.shift1, factory.shift2, factory.shift3],
       callback,
     );
   },
+  updatefactory: function(factory,callback){
+    return db.query(
+      "Update factory set name=?, subbusiness=?, shift1=?, shift2=?, shift3=? where id=?",   [factory.name, factory.business, factory.shift1, factory.shift2, factory.shift3, factory.id],
+      callback,
+    );
+  }
 };
 
 module.exports = Factory;
